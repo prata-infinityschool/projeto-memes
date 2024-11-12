b36566992c7f78e044c11a4c06cec9e143ff08bd
prata-infinityschool <133916154+prata-infinityschool@users.noreply.github.com>
2024-11-12 18:43:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+async function carregarMemes() {
+    const url = 'https://670edf663e7151861656043e.mockapi.io/api/memes'
+    const resposta = await fetch(url)
+    const informacoesMemes = await resposta.json()
+    const divGaleria = document.querySelector('#galeria')
+    divGaleria.innerHTML = ''
+    informacoesMemes.forEach(meme => {
+        const htmlDoMeme = `
+        <div class="card">
+            <h3>${meme.titulo}</h3>
+            <img src="${meme.linkImagem}" alt="">
+            <p>${meme.dataCadastro}</p>
+            <p>${meme.likes}</p>
+            <button class="btn-editar">Editar</button>
+            <button class="btn-apagar">Apagar</button>
+        </div>`
+        divGaleria.innerHTML += htmlDoMeme
+    });
+}
+
+carregarMemes()
